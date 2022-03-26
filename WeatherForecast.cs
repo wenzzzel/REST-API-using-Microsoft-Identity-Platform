0a6e2798910a29926d6184bc2172fab7c8fc0239
wenzzzel <wenzzzel@live.se>
2022-03-26 18:12:43 +0100

    dotnet new webapi -f net6.0 -au IndividualB2C

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace REST_API_using_Microsoft_Identity_Platform;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
