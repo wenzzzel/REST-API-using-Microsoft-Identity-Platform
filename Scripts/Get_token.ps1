478f87188a864f64559e63b8f1206c122f034b4f
wenzzzel <wenzzzel@live.se>
2022-03-26 22:00:58 +0100

    Changed to v2

--- Scripts/Get_token.ps1
+++ Scripts/Get_token.ps1
@@ -1,13 +1,14 @@
 $clientSecret = "<paste secret here>";
 $tenantId = "49ab38d6-5434-441d-b3ba-a9c3ed95610d"
 $clientId = "18d4fa8e-d5a2-446e-a111-04701e0de11b"
-$Uri = "https://login.microsoftonline.com/$tenantId/oauth2/token/"
+$scope = "api://18d4fa8e-d5a2-446e-a111-04701e0de11b/.default"
+$Uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token/"
 
 $body = @{
     'grant_type' = 'client_credentials'
     'client_id' = $clientId
     'client_secret' = $clientSecret
-    'resource' = $clientId
+    'scope' = $scope
 }
 
 Invoke-RestMethod `
