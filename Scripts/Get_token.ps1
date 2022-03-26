cff38ac69673c6d80a6c1d4e3f5160eef1d5a28f
wenzzzel <wenzzzel@live.se>
2022-03-26 22:01:35 +0100

    Secret isnt very secret

--- Scripts/Get_token.ps1
+++ Scripts/Get_token.ps1
@@ -1,4 +1,4 @@
-$clientSecret = "<paste secret here>";
+$clientSecret = "GwJ7Q~7TJpohh8-TAR1Q1viCoRxcAOAeIr9s6";
 $tenantId = "49ab38d6-5434-441d-b3ba-a9c3ed95610d"
 $clientId = "18d4fa8e-d5a2-446e-a111-04701e0de11b"
 $scope = "api://18d4fa8e-d5a2-446e-a111-04701e0de11b/.default"
